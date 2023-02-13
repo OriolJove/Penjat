688940a6b2dad7e626c7b7ea064f872c47fba5f8
Oriol Jové <jove.naranjo.oriol@alumnat.copernic.cat>
2023-02-13 13:07:58 +0100

    quita exclamacion

--- OneDrive/Documentos/NetBeansProjects/MiProyecto3/src/miproyecto3/MiProyecto3.java
+++ OneDrive/Documentos/NetBeansProjects/MiProyecto3/src/miproyecto3/MiProyecto3.java
@@ -14,7 +14,7 @@ public class MiProyecto3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola aplicació!");
+        System.out.println("Hola aplicació");
     }
     
 }

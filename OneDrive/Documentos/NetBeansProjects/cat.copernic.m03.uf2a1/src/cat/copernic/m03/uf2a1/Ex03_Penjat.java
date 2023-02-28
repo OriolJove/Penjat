a1d759ebba1aba3cd458a879f4c8e17cb16424b8
Oriol Jové <jove.naranjo.oriol@alumnat.copernic.cat>
2023-02-28 08:39:03 +0100

    penjat1

---
+++ OneDrive/Documentos/NetBeansProjects/cat.copernic.m03.uf2a1/src/cat/copernic/m03/uf2a1/Ex03_Penjat.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.m03.uf2a1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author orjon
+ */
+public class Ex03_Penjat {
+    public static void main(String[] args) {
+        
+        final char[][] estatPenjatInicial =
+        {
+          {' ',' ',' ',' ','_','_','_','_',' ',' ',' ',' '},                                      
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' '},
+          {' ','_','_','|','_',' ',' ',' ',' ',' ',' ',' '},                                      
+          {'|',' ',' ',' ',' ','|','_','_','_','_','_',' '},
+          {'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|'},
+          {'|','_','_','_','_','_','_','_','_','_','_','|'}
+         };        
+    }
+    
+    
+    static void mostrarEstatPenjat(char[][] estat) {
+        
+    }
+    
+    static void inicialitzarEstatPenjat(char[][] estatPenjatIni, char[][] estat) {
+    
+    }
+    
+    static void mostrarParaula(String paraula, boolean[] encertades) {
+        
+    }
+    
+    static void mostrarLletresIntroduides(String lletres) {
+        
+    }
+    
+    static String demanarLletra(String lletres) {
+        
+        return null;
+    
+    }
+    
+    static boolean existeixLletra(String lletres, char lletra) {
+        
+        return true;
+    
+    }
+    
+    static void actualitzarEstatPenjat(char[][] penjat,int errors) {
+        
+    }
+    
+    static void netejaPantalla() {
+    
+    }
+    
+    
+    
+}
